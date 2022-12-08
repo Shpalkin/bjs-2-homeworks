fae61fd0282b4502a8dd4cbd8da7031fd9dabc1c
Maria Bugaeva <116347809+Shpalkin@users.noreply.github.com>
2022-12-08 13:35:49 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,24 @@
+//Задача 1
+
 function compareArrays(arr1, arr2) {
-   let result = arr1.length == arr2.length && arr1.every((item, i) => item === arr2[i]);
-   return result;
+
+
+  let result = (arr1.length == arr2.length) && arr1.every((item, index) =>
+    item === arr2[index]); 
+  return result; 
 }
+compareArrays([8, 9], [8, 9])
+
+
+// Задача 2
 
 function advancedFilter(arr) {
-   let resultArr = arr.filter(number => number > 0 && number % 3 === 0).map(number => number * 10);
-   return resultArr;
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
+
+  return resultArr; 
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pri
