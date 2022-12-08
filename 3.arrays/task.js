15b51ed75acf48728628cbfabae8b8ab2cd82b47
Maria Bugaeva <116347809+Shpalkin@users.noreply.github.com>
2022-12-08 13:34:26 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+   let result = arr1.length == arr2.length && arr1.every((item, i) => item === arr2[i]);
+   return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+   let resultArr = arr.filter(number => number > 0 && number % 3 === 0).map(number => number * 10);
+   return resultArr;
 }
